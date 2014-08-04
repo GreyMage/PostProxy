7152d448a9d6915eec0305baf236f58638e48b20
Eric Cutler <ericc@sylvane.com>
2014-08-04 17:24:53 -0400

    Adding channels

--- postproxy.js
+++ postproxy.js
@@ -6,16 +6,34 @@ var io = require('socket.io')(process.env.EMITPORT || 3001);
 var app = express();
 app.use(bodyParser.json()); // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({extended: true})); // to support URL-encoded bodies
+app.all("/*",function(req,res,next){
+	res.header('Access-Control-Allow-Origin' , "*"); // allow global posting.
+	res.header('Access-Control-Allow-Headers' , "*"); // allow global posting.
+	res.header('Access-Control-Allow-Headers' , "X-Requested-With, X-Broadcast-Channel"); // allow global posting.
+	res.header('Access-Control-Allow-Methods' , "GET, POST, PUT, DELETE, OPTIONS"); // allow global posting.
+	next();
+})
+
 app.post('/*', function(req, res) {
-	res.header('Access-Control-Allow-Origin' , "*");
-	io.sockets.emit("data",req.body);
+	console.log(req.headers);
+	var channel = req.headers['x-broadcast-channel'];
+	if(channel){
+		io.to(channel).emit("data",req.body);
+	} else {
+		io.sockets.emit("data",req.body);	
+	}
 	res.send('captured');
 });
-app.get('/penis', function(req, res) {
-	res.send('lewd');
-});
+
 app.get('/*', function(req, res) {
 	res.sendfile('index.html');
 });
 
 app.listen(process.env.RECVPORT || 3000);
+
+io.on('connection',function(socket){
+	socket.on('join',function(room){
+		console.log("Socket joined "+room);
+		socket.join(room);
+	});
+});
