04c67572244ee62b6e0a0ebc082a14f07dc2b6b6
Eric Cutler <ericc@sylvane.com>
2014-08-05 16:58:13 -0400

    Consolidating functionality to one port, adding password protection

--- postproxy.js
+++ postproxy.js
@@ -1,35 +1,48 @@
-var express = require('express');
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
 var bodyParser = require('body-parser');
-var io = require('socket.io')(process.env.EMITPORT || 3001);
+
+var CONNECTION_PASSWORD = 'sylvane';
 
 // Data Input
-var app = express();
 app.use(bodyParser.json()); // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({extended: true})); // to support URL-encoded bodies
 app.all("/*",function(req,res,next){
 	res.header('Access-Control-Allow-Origin' , "*"); // allow global posting.
 	res.header('Access-Control-Allow-Headers' , "*"); // allow global posting.
-	res.header('Access-Control-Allow-Headers' , "X-Requested-With, X-Broadcast-Channel"); // allow global posting.
+	res.header('Access-Control-Allow-Headers' , "X-Requested-With, X-Broadcast-Channel, X-Connection-Password"); // allow global posting.
 	res.header('Access-Control-Allow-Methods' , "GET, POST, PUT, DELETE, OPTIONS"); // allow global posting.
 	next();
 })
 
 app.post('/*', function(req, res) {
-	console.log(req.headers);
-	var channel = req.headers['x-broadcast-channel'];
-	if(channel){
-		io.to(channel).emit("data",req.body);
-	} else {
-		io.sockets.emit("data",req.body);	
+
+	var auth = req.headers['x-connection-password'];
+	if(auth != CONNECTION_PASSWORD){
+		res.send('1');
+		return;
 	}
-	res.send('captured');
+
+	var channel = req.headers['x-broadcast-channel'];
+	if(!channel){
+		res.send('2');
+		return;
+	}
+
+	console.log("Relayed",req.body,"to",channel);
+	io.to(channel).emit("data",req.body);
+	res.send('0');
 });
 
-app.get('/*', function(req, res) {
+
+app.get('/', function(req, res) {
 	res.sendfile('index.html');
 });
 
-app.listen(process.env.RECVPORT || 3000);
+
+server.listen(process.env.PORT || 3000);
 
 io.on('connection',function(socket){
 	socket.on('join',function(room){
